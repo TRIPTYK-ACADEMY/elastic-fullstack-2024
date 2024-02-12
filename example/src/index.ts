184f9d4a4cfea474bae48c2fe20f51a4d7d90842
AmauryD <deflorenne.amaury@gmail.com>
2024-02-12 10:08:30 +0100

    push elastic

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,7 +1,21 @@
 import { Client } from '@elastic/elasticsearch';
+import { readFile } from 'fs/promises';
 
 async function init() {
-    
+    const client = new Client({
+        // l'url de votre serveur elastic
+        node: 'http://localhost:9200',
+        auth: {
+            username: 'elastic',
+            password: '2ctzzZXOYSn=+2PkJUET'
+        },
+        tls: {
+            ca: await readFile("http_ca.crt")
+        }
+    });
+
+    const isRunning = await client.ping();
+    console.log(isRunning);
 }
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
